8333972fb5a6070727e07a909004ede2e172e240
Boris <bokan.boris@gmail.com>
2018-12-25 03:42:24 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ec2-auto-shutdown
+
+*ec2-auto-shutdown* is a systemd service that helps you reduce AWS costs. 
+It will shut down an instance automatically when there is no SSH activity for configured period of time and average load is bellow configured threshold.
+
+## Installation
+
+```sh
+$ curl -o- https://raw.githubusercontent.com/bokan/ec2-auto-shutdown/master/install.sh | sudo bash
+```
+
+## Configuration
+
+Config file is located at */etc/ec2-auto-shutdown/ec2-auto-shutdown-config*.
+
+Restart service after making changes using:
+```sh
+$ sudo systemctl restart ec2-auto-shutdown
+```
+
---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+{ # this ensures the entire script is downloaded #
+
+  if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+  fi
+
+  service_install_path="/opt/ec2-auto-shutdown"
+  config_install_path="/etc/ec2-auto-shutdown"
+  systemd_service_unit_install_path="/etc/systemd/system"
+
+  github_url="https://raw.githubusercontent.com/bokan/ec2-auto-shutdown/master"
+  service_path="/ec2-auto-shutdown"
+  config_path="/ec2-auto-shutdown-config"
+  systemd_service_unit_path="/ec2-auto-shutdown.service"
+
+
+  make_dir_and_verify() {
+
+    mkdir -p $1
+    if [ ! -d $1 ]; then
+      echo "Unable to create $1"
+      exit
+    fi
+
+  }
+
+  download_and_verify() {
+
+    curl -o $1 $2
+
+    if [ ! -f $1 ]; then
+      echo "Download of $2 failed."
+      exit
+    fi
+
+  }
+
+  make_dir_and_verify $service_install_path
+  make_dir_and_verify $config_install_path
+
+  download_and_verify $service_install_path$service_path $github_url$service_path 
+  download_and_verify $config_install_path$config_path $github_url$config_path 
+  download_and_verify $systemd_service_unit_install_path$systemd_service_unit_path $github_url$systemd_service_unit_path 
+
+  chmod +x $service_install_path$service_path
+  systemctl enable ec2-auto-shutdown.service 
+  systemctl start ec2-auto-shutdown.service 
+
+  echo "ec2-auto-shutdown is installed."
+
+} # this ensures the entire script is downloaded #
+
